aa07628b969a253e987595cf605c58347017b22d
VECTOR <chukwukaepundu@gmail.com>
2025-12-29 12:53:29 +0100

    Todo list

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./todol-list.html",
+    "./todo.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
